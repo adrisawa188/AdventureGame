1a02dc1be2d83c0ffea80546d35b17264e6b06ad
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2021-05-17 14:11:22 -0400

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.imageBox = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(651, 104);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // option2Button
+            // 
+            this.option2Button.Image = global::AdventureGame.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_right;
+            this.option2Button.Location = new System.Drawing.Point(12, 354);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(34, 33);
+            this.option2Button.TabIndex = 2;
+            this.option2Button.UseVisualStyleBackColor = true;
+            // 
+            // option3Button
+            // 
+            this.option3Button.Image = global::AdventureGame.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_right;
+            this.option3Button.Location = new System.Drawing.Point(12, 393);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(34, 33);
+            this.option3Button.TabIndex = 3;
+            this.option3Button.UseVisualStyleBackColor = true;
+            // 
+            // option1Label
+            // 
+            this.option1Label.Location = new System.Drawing.Point(52, 315);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(539, 33);
+            this.option1Label.TabIndex = 4;
+            this.option1Label.Text = "label1";
+            this.option1Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // option2Label
+            // 
+            this.option2Label.Location = new System.Drawing.Point(52, 354);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(539, 33);
+            this.option2Label.TabIndex = 5;
+            this.option2Label.Text = "label2";
+            this.option2Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // option3Label
+            // 
+            this.option3Label.Location = new System.Drawing.Point(52, 393);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(539, 33);
+            this.option3Label.TabIndex = 6;
+            this.option3Label.Text = "label3";
+            this.option3Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(12, 113);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(651, 199);
+            this.imageBox.TabIndex = 7;
+            // 
+            // option1Button
+            // 
+            this.option1Button.Image = global::AdventureGame.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_arrow_right;
+            this.option1Button.Location = new System.Drawing.Point(12, 315);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(34, 33);
+            this.option1Button.TabIndex = 1;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(675, 450);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.Label imageBox;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You are walking through a forest when you realize it is getting dark.";
+
+                    option1Button.Text = "Do you want to spen the night";
+                    option2Button.Text = "Do you want to go back";
+                    option3Button.Enabled = false;
+                    break;
+                case 2:
+                    outputLabel.Text = "When you wake up the next day, you can see the path and make it out safely. Do you want to play again";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 3:
+                    outputLabel.Text = "You try to go back the way you came but you can not see the path";
+
+                    option1Button.Text = "Set up camp";
+                    option2Button.Text = "Keep going";
+                    option3Button.Enabled = false;
+                    break;
+                case 4:
+                    outputLabel.Text = "When you wake up you realize you are lost";
+
+                    option1Button.Text = "Lookj for food";
+                    option2Button.Text = "Try to find path";
+                    option3Button.Enabled = true;
+                    option3Button.Text = "Build fire";
+                    break;
+                case 5:
+                    outputLabel.Text = "You come accross some berries, they look familiar and you don't think they are poisonous";
+
+                    option1Button.Text = "Eat them";
+                    option2Button.Text = "Do not";
+                    option3Button.Enabled = false;
+                    break;
+                case 6:
+                    outputLabel.Text = "You rummage around collecting sticks and manage to start a fire";
+
+                    option1Button.Text = "Sit around the fire";
+                    option2Button.Text = "Try to find the path";
+                    option3Button.Enabled = false;
+                    break;
+                case 7:
+                    outputLabel.Text = "You feel replenished";
+
+                    option1Button.Text = "Find path";
+                    option2Button.Text = "Go back to camp";
+                    option3Button.Enabled = false;
+                    break;
+                case 8:
+                    outputLabel.Text = "You decide it is too dangerous to eat them";
+
+                    option1Button.Text = "Try to find more food";
+                    option2Button.Text = "Go back to camp";
+                    option3Button.Enabled = false;
+                    break;
+                case 9:
+                    outputLabel.Text = "You decide you should try to find the path";
+
+                    option1Button.Text = "Go north";
+                    option2Button.Text = "Go south";
+                    option3Button.Enabled = false;
+                    break;
+                case 10:
+                    outputLabel.Text = "You find the path and make it out safely. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 11:
+                    outputLabel.Text = "You look for hours but do not find the path";
+
+                    option1Button.Text = "Keep looking";
+                    option2Button.Text = "Head back";
+                    option3Button.Enabled = false;
+                    break;
+                case 12:
+                    outputLabel.Text = "As you start heading back, you step in a hole and break your leg. as you lay there, you here what appears to be a car in the direction you were heading. Unable to move, you lay there and die just short of saftey. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 13:
+                    outputLabel.Text = "You come accross a road and flag down an approaching vehicle and you make it to saftey. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 14:
+                    outputLabel.Text = "By the time you get back to camp it is getting dark.";
+
+                    option1Button.Text = "Build fire";
+                    option2Button.Text = "Got to sleep";
+                    option3Button.Enabled = false;
+                    break;
+                case 15:
+                    outputLabel.Text = "You build a fire and sit around for a while before going to bed. You are woken by footsteps. Someone saw the smoke of your fire and found you, they lead you out of the woods safely. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 16:
+                    outputLabel.Text = "You head back to camp on an empty stomach. As you walk, you are attacked by a couger and cannot escape because you are too tired. You die. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 17:
+                    outputLabel.Text = "As you search you come accross some bluberries which you eat. It is getting dark by the time you finnish, an you don't know if you can find camp.";
+
+                    option1Button.Text = "Try to find camp";
+                    option2Button.Text = "Stay put";
+                    option3Button.Enabled = false;
+                    break;
+                case 18:
+                    outputLabel.Text = "It is a particularly cold night, and you freeze to death without shelter. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 19:
+                    outputLabel.Text = "You find camp.";
+
+                    option1Button.Text = "Build a fire";
+                    option2Button.Text = "Go to sleep";
+                    option3Button.Enabled = false;
+                    break;
+                case 20:
+                    outputLabel.Text = "It is a particularly cold night and you freeze to death without a fire to keep you warm. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 21:
+                    outputLabel.Text = "As you look around you camp trying to decide witch way to go, you see a small clearing not far from you. You investigate and it is a path. You make it out safely. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 22:
+                    outputLabel.Text = " As you sit around the fire contemplating what to do next, you hear footsteps, someone saw the smoke from your fire and came to rescue you. You make it out safely. Do you want to play again.";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 23:
+                    outputLabel.Text = "As you look for the path, you come accross what appears to be some kind of den.";
+
+                    option1Button.Text = "Investigate";
+                    option2Button.Text = "Go back to camp";
+                    option3Button.Enabled = false;
+                    break;
+                case 24:
+                    outputLabel.Text = "The den belongs to a mother bear who promtply charges and kills you. Do you want to play again?.";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 25:
+                    outputLabel.Text = "After searching for hours with no luck, you go back to camp. When you arrive someone is there. They tell you they saw the smoke from your fire and came to help. They guide you to safety. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 26:
+                    outputLabel.Text = "The berries were poisonous and you die. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 27:
+                    outputLabel.Text = "It is a particularly cold night and you freeze to death without a fire to keep you warm. Do you want to play gain?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 28:
+                    outputLabel.Text = "A you stumble through the darkness, you trip and break your leg. A bear sense your distress and attacks you. You die. Do you want to play again?";
+
+                    option1Button.Text = "Yes";
+                    option2Button.Text = "No";
+                    option3Button.Enabled = false;
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing.";                
+                    break;
+            }           
+        }
+    }
+}
